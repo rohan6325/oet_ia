d70d1ce44107cf34b4bbaeb54c28b2fed9ac4443
Rohan Sharma <sharma.rs@somaiya.edu>
2023-10-26 03:18:32 +0530

    second

---
+++ lib/Models/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color correct = Color(0xFF00FF00);
+const Color incorrect = Color(0xFFFF0000);
+const Color neutral = Color.fromARGB(255, 184, 183, 183);
+const Color background = Color.fromARGB(255, 4, 7, 73);
